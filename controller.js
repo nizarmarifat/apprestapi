360867294efd06d0bc09a8b7b2ec5818c1942311
nizarmarifat <nizar.marifat@socketspace.com>
2021-10-12 10:37:04 +0700

    menampilkan data berdasarkan id

--- controller.js
+++ controller.js
@@ -3,19 +3,33 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
-    response.ok("Aplikasi REST API ku berjalan!",res)
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API ku berjalan!", res)
 }
 
 
 // menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req,res){
-    connection.query('SELECT * FROM mahasiswa', function(error,rows,fileds) {
-        if(error){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
             connection.log(error)
         }
-        else{
+        else {
             response.ok(rows, res)
         }
     })
+}
+//menampilkan data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
+        function (error, rows, fileds) {
+            if (error) {
+                console.log(error)
+            }
+            else {
+                response.ok(rows, res)
+            }
+        }
+    )
 }
